65fd5ce4ae6deaff6b57df40344afe9acee32085
vitoriaferreiranoquele <161841545+vitoriaferreiranoquele@users.noreply.github.com>
2024-11-01 10:32:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é uma função em programação?',
+    'Bloco de código reutilizável para uma tarefa específica.'
+)
+
+criaCartao(
+    'Matematica',
+    'O que é o número primo? ',
+    'um número primo é um número maior que 1 que só pode ser dividido por 1 e por ele mesmo, como 2,3,5,7, e 11.'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é a função de um loop for?',
+    'Repete uma ação várias vezes.'
+)
+
+criaCartao(
+    'Matematica',
+    'O que é a média ponderada?',
+    'Calcula-se considerando pesos diferentes para cada valor. '
+)
